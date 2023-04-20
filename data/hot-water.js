23d57e1efa18ab505d4a106ec010ac8e1421deab
Esteban____ <esteban.diaz@ludycom.com>
2023-04-20 14:41:01 -0500

    Final base

--- data/hot-water.js
+++ data/hot-water.js
@@ -1,63 +1,133 @@
-const dataJson = {
+dataJson = {
+
+
+
   productsData: [
+
     {
-      idProduct: 1,
+
+      idProduct: "1medidorChorroUnicoModeloCombi",
+
       nameProduct: "Medidor chorro único modelo Combi",
+
       detailGeneral: [
-        { detailDescription: "DN13 Q3 2,5 m3 /h R125" },
-        { detailDescription: "DN15 Q3 2,5 m3 /h R125" },
-        { detailDescription: "DN20 Q3 4 m3 /h R125", borderLine: true },
-        { detailDescription: "Módulo B" },
-        { detailDescription: "nº TCM 142/13-5128", borderLine: true },
-        { detailDescription: "Módulo D" },
-        { detailDescription: "nº 0119-SJ-A002-10" },
+
+        { detailDescription: 'DN13 Q3 2,5 m3 /h R125' },
+
+        { detailDescription: 'DN15 Q3 2,5 m3 /h R125' },
+
+        { detailDescription: 'DN20 Q3 4 m3 /h R125', borderLine: true },
+
+        { detailDescription: 'Módulo B' },
+
+        { detailDescription: 'nº TCM 142/13-5128', borderLine: true },
+
+        { detailDescription: 'Módulo D' },
+
+        { detailDescription: 'nº 0119-SJ-A002-10' }
+
       ],
+
       detailList: [
-        { characteristic: "Ratio: R125" },
+
+        { characteristic: 'Ratio: R125' },
+
         { characteristic: 'Dimensiones: 1/2" a 3/4"' },
-        { characteristic: "Totalizador: versión 45 grados" },
-        { characteristic: "Registro: policarbonato" },
-        { characteristic: "Cuerpo: Latón" },
-        { characteristic: "Transmisión: Magnética" },
+
+        { characteristic: 'Totalizador: versión 45 grados' },
+
+        { characteristic: 'Registro: policarbonato' },
+
+        { characteristic: 'Cuerpo: Latón' },
+
+        { characteristic: 'Transmisión: Magnética' }
+
       ],
-      imagen: {
-        src: "assets/img/products/water-meter/water-industry/medidorChorroUnicoModeloCombi.png",
-        alt: "imagen de Medidor chorro único modelo Combi",
+
+      imagenBig: {
+
+        src: 'assets/img/products/water-meter/water-industry/medidorChorroUnicoModeloCombi.png',
+
+        alt: 'imagen Grande de Medidor chorro único modelo Combi'
+
       },
+
+      imagenSmall: {
+
+        src: 'assets/img/products/water-meter/water-industry/small/medidorUltrasonicoModeloJU5050.png',
+
+        alt: 'imagen pequeña de Medidor ultrasónico modelo JU550'
+
+      },
+
+      pdf: "assets/pdf/water-meter/water-industry/catalogoJM600.pdf"
+
     },
+
     {
-      idProduct: 2,
+
+      idProduct: "2MedidorUltrasonicoModeloJU550",
+
       nameProduct: "Medidor ultrasónico modelo JU550",
+
       detailGeneral: [
-        { detailDescription: "Temperaturas almacenamiento" },
-        { detailDescription: "20°C a + 70°C(max. 2 semanas con T > 35°C)" },
-        {
-          detailDescription: "13°F a + 158°F(max. 2 semanas con T > 95°F)",
-          borderLine: true,
-        },
-        { detailDescription: "Temperatura agua " },
-        {
-          detailDescription: "0.1°C a + 50°C a 32°F a + 122°F",
-          borderLine: true,
-        },
-        { detailDescription: "Temperatura ambiente" },
-        { detailDescription: "1°C a + 70°C a 34°F a + 158°F" },
+
+        { detailDescription: 'Temperaturas almacenamiento' },
+
+        { detailDescription: '20°C a + 70°C(max. 2 semanas con T > 35°C)' },
+
+        { detailDescription: '13°F a + 158°F(max. 2 semanas con T > 95°F)', borderLine: true },
+
+        { detailDescription: 'Temperatura agua ' },
+
+        { detailDescription: '0.1°C a + 50°C a 32°F a + 122°F', borderLine: true },
+
+        { detailDescription: 'Temperatura ambiente' },
+
+        { detailDescription: '1°C a + 70°C a 34°F a + 158°F' }
+
       ],
+
       detailList: [
-        { characteristic: "Ratio: R500" },
+
+        { characteristic: 'Ratio: R500' },
+
         { characteristic: 'Dimensiones: 1 / 2" a 1 1/2"' },
-        { characteristic: "Totalizador: versión plana" },
-        { characteristic: "Registro: electrónico" },
-        { characteristic: "Cuerpo: Latón" },
-        {
-          characteristic:
-            "Comunicación: Emisor de pulsos o M - bus cable / Wireless M - Bus / Wireless M - Bus - LoRa WAN Integrado * Batería de litio con una vida útil hasta 16 años",
-        },
+
+        { characteristic: 'Totalizador: versión plana' },
+
+        { characteristic: 'Registro: electrónico' },
+
+        { characteristic: 'Cuerpo: Latón' },
+
+        { characteristic: 'Comunicación: Emisor de pulsos o M - bus cable / Wireless M - Bus / Wireless M - Bus - LoRa WAN Integrado * Batería de litio con una vida útil hasta 16 años' }
+
       ],
-      imagen: {
-        src: "assets/img/products/water-meter/water-industry/medidorUltrasonicoModeloJU5050.png",
-        alt: "imagen de Medidor chorro único modelo Combi",
+
+      imagenBig: {
+
+        src: 'assets/img/products/water-meter/water-industry/medidorUltrasonicoModeloJU5050.png',
+
+        alt: 'imagen grande de Medidor ultrasónico modelo JU550'
+
       },
-    },
-  ],
+
+      imagenSmall: {
+
+        src: 'assets/img/products/water-meter/water-industry/small/medidorUltrasonicoModeloJU5050.png',
+
+        alt: 'imagen pequeña de Medidor ultrasónico modelo JU550'
+
+      },
+
+      pdf: "assets/pdf/catalogoJM600.pdf"
+
+
+
+    }
+
+
+
+  ]
+
 };
